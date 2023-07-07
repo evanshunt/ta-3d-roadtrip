fa9d239d05198ceec0337aa51e07d4ec81552d20
Mike Quinn <mprquinn@gmail.com>
2023-07-07 13:54:01 -0600

    🚧  (Scroll) Adds ScrollControls

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,40 +1,34 @@
 import Canmore from "./models/Canmore.jsx";
 import React, { useRef, useState } from "react";
-import { Environment, OrbitControls, Sky, Stage } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  Scroll,
+  ScrollControls,
+  Sky,
+  Stage,
+  useScroll,
+} from "@react-three/drei";
 import { useControls } from "leva";
 import { Perf } from "r3f-perf";
+import { useFrame } from "@react-three/fiber";
 
 const Experience = () => {
-  const { sunPosition } = useControls("sky", {
-    sunPosition: { value: [1, 2, 3] },
-  });
   const { lightIntensity } = useControls("light", {
     lightIntensity: { value: 0.75, min: 0, max: 10 },
   });
 
   return (
     <>
-      <Sky distance={450000} sunPosition={sunPosition} />
       <Perf position="top-left" />
-      <OrbitControls makeDefault={true} />
-      <Stage adjustCamera={true}>
-        <ambientLight />
-        <directionalLight
-          castShadow
-          intensity={lightIntensity}
-          position={sunPosition}
-          shadow-mapSize={[1024, 1024]}
-          shadow-camera-near={1}
-          shadow-camera-far={10}
-          shadow-camera-top={5}
-          shadow-camera-right={5}
-          shadow-camera-bottom={-5}
-          shadow-camera-left={-5}
-        >
+      {/* <OrbitControls makeDefault={true} /> */}
+      <ScrollControls damping={0.1}>
+        <Stage adjustCamera={true}>
+          <ambientLight intensity={lightIntensity} />
           <Environment background={false} files={"/env.hdr"} />
           <Canmore castShadow receiveShadow />
-        </directionalLight>
-      </Stage>
+        </Stage>
+      </ScrollControls>
     </>
   );
 };

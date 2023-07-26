da820f19f66c79d0d16a49858c19bc6ed5bf994d
Mike Quinn <mprquinn@gmail.com>
2023-07-26 15:00:12 -0600

    ✨  (Adds new model, baked texture and tilt shift shader)

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,17 +1,56 @@
-import { createRoot } from 'react-dom/client'
-import React, { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Stage } from '@react-three/drei'
+import { createRoot } from "react-dom/client";
+import React, { useRef, useState } from "react";
+import { useControls } from "leva";
+import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
+import TiltShiftEffects from "./shaders/tiltshift.jsx";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
 
-import Canmore from './models/Canmore.jsx'
+import Canmore from "./models/Canmore.jsx";
+import LakeLouise from "./models/LakeLouise.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <OrbitControls autoRotate={true} makeDefault={true} />
-    <Stage adjustCamera={true}>
-      <Canmore />
-    </Stage>
-  </Canvas>,
-)
+const App = () => {
+  const { focusDistance, focalLength, bokehScale } = useControls({
+    focusDistance: {
+      min: 0,
+      max: 4,
+      value: 2,
+    },
+    focalLength: {
+      min: 0,
+      max: 1,
+      value: 0.1,
+    },
+    bokehScale: {
+      min: 0,
+      max: 10,
+      value: 2,
+    },
+  });
+  return (
+    <Canvas
+      camera={{
+        position: [-3, 8, 15],
+        fov: 40,
+      }}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} intensity={0.2} />
+      <OrbitControls autoRotate={false} makeDefault={true} />
+      {/* <Stage adjustCamera={true}> */}
+      {/* <Canmore /> */}
+      <LakeLouise />
+      <TiltShiftEffects />
+      {/* <EffectComposer>
+        <DepthOfField
+          focusDistance={focusDistance}
+          focalLength={focalLength}
+          bokehScale={bokehScale}
+        />
+      </EffectComposer> */}
+      {/* </Stage> */}
+    </Canvas>
+  );
+};
+
+createRoot(document.getElementById("root")).render(<App />);

09f746919f0755d1ed23af08f4ee78cf82ea3cae
Mike Quinn <mprquinn@gmail.com>
2023-08-02 14:25:35 -0600

    ⚡️  (Textures) Compresses baked texture

--- src/models/IceFields.jsx
+++ src/models/IceFields.jsx
@@ -6,7 +6,7 @@ import React, { useRef } from "react";
 import { Plane, useGLTF, useTexture } from "@react-three/drei";
 
 export default function IceFields(props) {
-  const iceFieldsTexture = useTexture("/textures/baked4-adjusted-ps.png");
+  const iceFieldsTexture = useTexture("/textures/baked4-adjusted-ps.jpg");
   const scene = useGLTF("/glb/ice-fields-normals.glb");
 
   return (

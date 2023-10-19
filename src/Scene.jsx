14ef28f80e649f5e49884b8736f44c8b32671622
Mike Quinn <mprquinn@gmail.com>
2023-10-19 09:59:49 -0600

    💄  (Info Boxes) Updates the info boxes so they have a darker BG

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -278,7 +278,8 @@ const Scene = (props) => {
         <Plane />
       </e.group>
 
-      <TiltShiftEffects />
+      {/* @TODO: figure out a way to use this in a less intrusive way. NOTE: the useframe in here is the only way that gsap animations play during pauses. there is also an issue with that loop causing a frame rate decrease due to the state being reset. */}
+      {/* <TiltShiftEffects /> */}
     </>
   );
 };
